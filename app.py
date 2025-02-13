1df99d7707ede21a59280b91599750e3b0c48dd4
ritikaaguptaa <ritika@korecent.com>
2025-02-13 15:46:53 +0530

    Database

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, jsonify
+
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# Configure the PostgreSQL database connection
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:admin@localhost/library'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Example model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"<User {self.name}>"
+
+# Route to add a user
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    data = request.get_json()
+    new_user = User(name=data['name'], email=data['email'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'User added successfully!'})
+
+# Route to get all users
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([{'id': user.id, 'name': user.name, 'email': user.email} for user in users])
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+from app import app, db
+
+with app.app_context():
+    db.create_all()
+    print("Tables created successfully!")
