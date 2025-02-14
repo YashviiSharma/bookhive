8e4048860ede8d161661ba1895f5eba02668fbd7
ritikaaguptaa <ritika@korecent.com>
2025-02-14 12:47:07 +0530

    DB Connection

--- app.py
+++ app.py
@@ -1,25 +1,10 @@
 
-
-from flask import render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import render_template, request, redirect
 from flask import Flask, jsonify
-
-
-from database import db, init_app
-from models import Book, Member, Transaction
+from models import db, Book, Member, Transaction, initialize_db
 
 app = Flask(__name__)
-
-# PostgreSQL Database Configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:admin@localhost/library'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# Initialize the database with the app
-init_app(app)
-
-@app.route("/")
-def index():
-    return "Home Page"
+initialize_db()
 
 if __name__ == '__main__':
     app.run(debug=True)
